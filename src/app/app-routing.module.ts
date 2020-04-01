a4080c3d28482dd0b534d4a8f25cb6e8de462cf6
Naveen Raghuvanshi <meoknu@gmail.com>
2020-04-01 17:56:18 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,28 @@
+import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'users',
+    component: UsersComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
